3ae5919368ac456e2ccfa5feeea4dd5a7b3bd345
aquifzubair2 <aquif.zunair@mountblue.tech>
2020-07-14 23:32:45 +0530

    implemented matchesPerYear and matchesWinPerTeamPerYear function

--- src/server/index.js
+++ src/server/index.js
@@ -11,19 +11,6 @@ csv()
     // console.log(jsonData);
     matchesPerYear(jsonData)
     matchesWonPerTeamPerYear(jsonData)
-    // let output = {};
-    // for(let item of jsonData){
-    //     if(output.hasOwnProperty(item.season)){
-    //         output[item.season]++
-    //     }
-    //     else
-    //         output[item.season] = 1;
-    // }
-    // console.log(output)
     
 })
 
-
- 
-// Async / await usage
-// const jsonArray=await csv().fromFile(csvFilePath);
